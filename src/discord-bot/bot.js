e2f30396f32d863b0ae1855aae92848cdcf9f14e
F <fahad.assoumani@epitech.eu>
2020-10-30 13:53:12 +0100

    [UPDATE] remove some useless console logs and stores channel and server infos in .env file

--- src/discord-bot/bot.js
+++ src/discord-bot/bot.js
@@ -2,36 +2,31 @@ const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+/* Discord commands loading */
 client.commands = new Discord.Collection();
-
 const commandFiles = fs.readdirSync(__dirname + '/commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-    console.log(file);
     const command = require(`./commands/${file}`);
-
     client.commands.set(command.name, command);
 }
-
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
 
 //TODO: Change this to env variables 
-const prefix = 'aer!';
-const SERV_ID = '438095103060344843'
-const CHAN_ID = '691671860148174898';
+const prefix = process.env.DISCORD_PREFIX;
+const SERV_ID = process.env.DISCORD_SERV_ID;
+const CHAN_ID = process.env.DISCORD_CHAN_ID;
 
 
-client.once('ready', () => console.log('Bot Is Ready'))
+client.once('ready', () => console.log('Bot Is Ready'));
 client.on('message', message => {
     if (message.guild.id !== SERV_ID || message.channel.id !== CHAN_ID) return;
     msg_content = message.content;
     
     if (!msg_content.startsWith(prefix) || message.author.bot) return;
 
-    console.log(msg_content);
     const args = msg_content.slice(prefix.length).trim().split(' ');
-
     const command = args.shift();
 
     if (!client.commands.has(command)) return;
