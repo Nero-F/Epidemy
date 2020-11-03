d3568b9445b44ef95ce502efc26d79e2c1652ed3
F <fahad.assoumani@epitech.eu>
2020-11-03 13:52:09 +0100

    [ADD] aer-only commands handling

--- src/discord-bot/bot.js
+++ src/discord-bot/bot.js
@@ -1,24 +1,33 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const { getAerObjListFromKey } = require('../aer');
 
 client.commands = new Discord.Collection();
+client.commandsAdm = new Discord.Collection();
 const commandFiles = fs.readdirSync(__dirname + '/commands').filter(file => file.endsWith('.js'));
+const commandFilesAdm = fs.readdirSync(__dirname + '/commands/adm').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     client.commands.set(command.name, command);
 }
+for (const file of commandFilesAdm) {
+    const commandAdm = require(`./commands/${file}`);
+    client.commandsAdm.set(commandAdm.name, commandAdm);
+}
+
+const aerListId = getAerObjListFromKey('discordId');
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const prefix = process.env.DISCORD_PREFIX;
+const prefix_admin = process.env.DISCORD_PREFIX_ADMIN;
 const SERV_ID = process.env.DISCORD_SERV_ID;
 const CHAN_ID = process.env.DISCORD_CHAN_ID;
 
 
 client.once('ready', () => console.log('Bot Is Ready'));
 client.on('message', message => {
-    console.log(message);
     if (message.guild == undefined || 
         message.guild.id !== SERV_ID || message.channel.id !== CHAN_ID) {
         message.author.send(`You can only call me in this specific channel ${CHAN_ID}...`);
@@ -26,15 +35,33 @@ client.on('message', message => {
     }
     msg_content = message.content;
     
-    if (!msg_content.startsWith(prefix) || message.author.bot) return;
+    if ((!msg_content.startsWith(prefix) && !msg_content.startsWith(prefix_admin))|| message.author.bot) return;
 
-    const args = msg_content.slice(prefix.length).trim().split(' ');
+    let w_collection;
+    let w_prefix = "";
+
+    if (msg_content.startsWith(prefix)) {
+        w_prefix = prefix;
+        w_collection = client.commands;
+    } else {
+        w_prefix = prefix_admin;
+        w_collection = client.commandsAdm;
+        if (!aerListId.has(message.author.id)) {
+            message.reply('You\'re not part of the AER\'s GVNG so you cannot access to these commands :P');
+            return;
+        }
+    }
+    console.log(message.author);
+
+    const args = msg_content.slice(w_prefix.length).trim().split(' ');
     const command = args.shift();
 
-    if (!client.commands.has(command)) return;
-
+    if (!w_collection.has(command)) {
+        message.reply(`The command '${command}' does not exist... Please refer to ${w_prefix} help`);
+        return;
+    }
     try {
-        client.commands.get(command).execute(message, args);
+        w_collection.get(command).execute(message, args);
     } catch (error) {
         console.error(error);
         message.reply('There was an error trying to execute that command sry :(');
