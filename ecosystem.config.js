a9bac8cd81125e57fcdd1e40bd5ada370ac8ed88
F <fahad.assoumani@epitech.eu>
2021-02-24 20:14:55 +0100

    [CD][UPDATE] ecosystem pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,7 +13,7 @@ module.exports = {
       host : '152.228.134.62',
       ref  : 'origin/master',
       repo : 'git@github.com:Nero-F/Epidemy.git',
-      path : '/home/nero_f',
+      path : 'home/nero_f',
       'pre-deploy-local': '',
       'post-deploy' : 'npm install --production && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
