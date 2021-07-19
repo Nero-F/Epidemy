2fbab50e368490058bb2a5a58ffeda75b06a4d96
F <fahad.assoumani@epitech.eu>
2021-07-20 00:38:52 +0200

    update bot: cleaning some old stuffs (naming conventions..)

--- src/discord-bot/bot.js
+++ src/discord-bot/bot.js
@@ -19,25 +19,29 @@ for (const file of commandFilesAdm) {
 
 const aerListId = getAerObjListFromKey('discordId');
 
-const BOT_TOKEN = process.env.BOT_TOKEN;
+const BOT_TOKEN = process.env.NODE_ENV === "develop" ? process.env.BOT_TOKEN_DEV : process.env.BOT_TOKEN;
 const prefix = process.env.DISCORD_PREFIX;
-const prefix_admin = process.env.DISCORD_PREFIX_ADMIN;
+const prefixAdmin = process.env.DISCORD_PREFIX_ADMIN;
 
 client.once('ready', () => console.log('Bot Is Ready'));
 client.on('message', message => {
     const is_from_aer = aerListId.find(id => id === message.author.id) == undefined ? false : true;
-    msg_content = message.content;
-    if ((!msg_content.startsWith(prefix) && !msg_content.startsWith(prefix_admin))|| message.author.bot) return;
+    const msgContent = message.content;
+
+    if ((!msgContent.startsWith(prefix) && !msgContent.startsWith(prefixAdmin)) || message.author.bot) return;
 
     let w_collection;
     let w_prefix = "";
 
-    if (msg_content.startsWith(prefix)) {
+    if (msgContent.startsWith(prefix)) {
         if (message.channel.type !== 'dm' && !is_from_aer) {
             message.reply(`You can only call me via DMs...`);
             return;
         }
-        w_prefix = prefix; w_collection = client.commands; } else { w_prefix = prefix_admin;
+        w_prefix = prefix;
+        w_collection = client.commands; 
+    } else {
+        w_prefix = prefixAdmin;
         w_collection = client.commandsAdm;
         if (!is_from_aer) {
             message.reply('You\'re not part of the AER\'s GVNG so you cannot access to these commands :P');
@@ -45,7 +49,7 @@ client.on('message', message => {
         }
     }
 
-    const args = msg_content.slice(w_prefix.length).trim().split(' ');
+    const args = msgContent.slice(w_prefix.length).trim().split(' ');
     const command = args.shift();
 
     if (!w_collection.has(command)) {
